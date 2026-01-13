19fa65cae6422169cfca7c3a41a137759bd0a9a0
AqssatCIT <aqssatcit23@gmail.com>
2026-01-13 22:32:22 +0200

    2026-01-132

--- resources/views/filament/pwa-head.blade.php
+++ resources/views/filament/pwa-head.blade.php
@@ -4,3 +4,6 @@
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
+<link rel="manifest" href="/icons/site.webmanifest">
+<link rel="apple-touch-icon" href="/icons/apple-touch-icon.png">
+<meta name="theme-color" content="#111827">
